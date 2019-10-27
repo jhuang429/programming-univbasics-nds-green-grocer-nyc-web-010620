c8fa8a29a5c9925f00d63c8d4be0f7d1dbb82325
Jerry Huang <jhuang9240@gmail.com>
2019-10-27 01:32:23 +0000

    Automatically backed up by Learn

--- test.rb
+++ test.rb
@@ -1,33 +1,46 @@
-test = [
-  {:item => "AVOCADO", :price => 3.00, :clearance => true },
-  {:item => "AVOCADO", :price => 3.00, :clearance => true },
-  {:item => "KALE", :price => 3.00, :clearance => false},
-  {:item => "KALE", :price => 3.00, :clearance => false},
-  {:item => "KALE", :price => 4.00, :clearance => false},
-  {:item => "PB", :price => 3.00, :clearance => false}
+test1 = [
+  {:item => "AVOCADO", :price => 3.00, :clearance => true, :count => 3},
+  {:item => "KALE",    :price => 3.00, :clearance => false, :count => 1}
 ]
 
-def consolidate_cart(cart)
-  item_count = 0
-  result = {}
+test2 = [{:item => "AVOCADO", :num => 2, :cost => 5.00}]
 
+
+def apply_coupons(cart, coupons)
+  item_count = 0
+  result = []
   while item_count < cart.length do
+    coupon_count = 0
     item = cart[item_count][:item]
-    result[item] = {count: 0}
+    while coupon_count < coupons.length do
+      if item == coupon[coupon_count][:name] #does cart item match any coupons?
+        coupon_qty = cart[item_count][:num]
+        if cart[item_count][:num] >= coupon_qty
+          new_price = coupon[coupon_count][:price] / coupon_qty
+          result.push({item: "#{item} W/COUPON" price: new_price, clearance: cart[item_count][:clearance], count: 0})
+          while cart[item_count][:count] >= coupon_qty do #does qty match requirments?
+            result[-1][:count] += coupon_qty
+            cart[item_count][:count] - coupon_qty
+          end
+
+        if cart[item_count] > 0
+          result.push(cart[item_count]) #push remaining qtys into results
+          cart.delete_at(item_count)
+        end
+
+        if cart[item_count] == 0
+          cart.delete_at(item_count)
+
+        end
+      end #if coupon match name end statement
+
+      coupons += 1
+    end
+
     item_count += 1
   end
-
-  item_count = 0
-  while item_count < cart.length do
-    item = cart[item_count][:item]
-    result[item][:count] += 1
-    result[item][:clearance] = cart[item_count][:clearance]
-    result[item][:price] = cart[item_count][:price]
-    item_count += 1
-  end
-
+  result.push(cart)
   result
-
 end
 
-puts consolidate_cart(test)
+puts apply_coupons(test1, test2)
