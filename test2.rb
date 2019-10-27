2fc93830eebaa8fc4df6990d2540aa536b3a0775
Jerry Huang <jhuang9240@gmail.com>
2019-10-27 02:09:02 +0000

    Automatically backed up by Learn

--- test2.rb
+++ test2.rb
@@ -24,7 +24,6 @@ end
 
 test = [
   {:item => "AVOCADO", :price => 3.00, :clearance => true },
-  {:item => "AVOCADO", :price => 3.00, :clearance => true },
   {:item => "KALE", :price => 3.00, :clearance => false}
 ]
 
