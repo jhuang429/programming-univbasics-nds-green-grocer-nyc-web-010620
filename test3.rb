9e5bb12a9db6b956ec2cd6687764ba0b09ae38a6
Jerry Huang <jhuang9240@gmail.com>
2019-10-27 02:13:20 +0000

    Automatically backed up by Learn

---
+++ test3.rb
@@ -0,0 +1,25 @@
+def find_item_by_name_in_collection(name, collection)
+  item_count = 0
+  while item_count < collection.length do
+    if name == collection[item_count][:item]
+      result = collection[item_count]
+    end
+    item_count += 1
+  end
+  result
+end
+
+list = [
+  {:item => "AVOCADO", :price => 3.00, :clearance => true},
+  {:item => "KALE", :price => 3.00, :clearance => false},
+  {:item => "BLACK_BEANS", :price => 2.50, :clearance => false},
+  {:item => "ALMONDS", :price => 9.00, :clearance => false},
+  {:item => "TEMPEH", :price => 3.00, :clearance => true},
+  {:item => "CHEESE", :price => 6.50, :clearance => false},
+  {:item => "BEER", :price => 13.00, :clearance => false},
+  {:item => "PEANUTBUTTER", :price => 3.00, :clearance => true},
+  {:item => "BEETS", :price => 2.50, :clearance => false},
+  {:item => "SOY MILK", :price => 4.50, :clearance => true}
+]
+
+puts find_item_by_name_in_collection("AVOCADO", list)
